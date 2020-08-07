efa810d2a6850226d8abda5ee4c6ff9000b6e6ed
Jack Gilcrest <jpag@jpag.kali-office>
2020-08-06 23:30:18 -0500

    initial

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,13 +2,8 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
-    },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
-    social: {
-      twitter: `kylemathews`,
+      name: `Blox Consulting LLC`,
+      summary: `Web technology consulting`,
     },
   },
   plugins: [
@@ -76,6 +71,7 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    'gatsby-plugin-netlify-cms'
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
